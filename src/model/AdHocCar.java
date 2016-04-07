97680ea9dcd2e1a409af0e2d807873adc5bd5cbb
Rik de Vries <rikdevries253@hotmail.nl>
2016-04-07 11:39:27 +0200

    MVC WIP

---
+++ src/model/AdHocCar.java
@@ -0,0 +1,12 @@
+package model;
+
+import controller.*;
+import main.*;
+import runner.*;
+import view.*;
+
+public class AdHocCar extends Car {
+    public AdHocCar() {
+
+    }
+}
---
+++ src/model/ParkingPass.java
@@ -0,0 +1,17 @@
+package model;
+
+import controller.*;
+import main.*;
+import runner.*;
+import view.*;
+
+/**
+ * Created by rik on 4/5/16.
+ */
+public class ParkingPass extends Car{
+
+
+    public ParkingPass(){
+
+    }
+}
